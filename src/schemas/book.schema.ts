236ee7b0f4c46a47915992f9d181b99ec065b652
Artyom Koroteev <artyomkoroteev@artyoms-macbook-air.home>
2023-08-07 09:43:39 +0200

    added connection to db

---
+++ src/schemas/book.schema.ts
@@ -0,0 +1,21 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
+export type BookDocument = HydratedDocument<Book>;
+
+@Schema()
+export class Book {
+  @Prop()
+  name: string;
+
+  @Prop()
+  description: string;
+
+  @Prop()
+  rating: number;
+
+  @Prop()
+  status: string;
+}
+
+export const BooksSchema = SchemaFactory.createForClass(Book);
